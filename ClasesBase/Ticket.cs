16097c4b9805e325714e57530427e8440abcf0cc
Ignacio Levy <ignalevy@gmail.com>
2020-09-10 21:18:16 -0300

    Commit de prueba
    No se cambio nada

--- ClasesBase/Ticket.cs
+++ ClasesBase/Ticket.cs
@@ -7,11 +7,47 @@ namespace ClasesBase
 {
     public class Ticket
     {
-        private int TICK_Nro;
-        private DateTime TICK_FechaVenta;
-        private int CLI_DNI;
-        private int PRO_Codigo;
-        private string BUT_Fila;
-        private string BUT_Nro;
+        private int tick_Nro;
+
+        public int Tick_Nro
+        {
+            get { return tick_Nro; }
+            set { tick_Nro = value; }
+        }
+        private DateTime tick_FechaVenta;
+
+        public DateTime Tick_FechaVenta
+        {
+            get { return tick_FechaVenta; }
+            set { tick_FechaVenta = value; }
+        }
+        private int cli_DNI;
+
+        public int Cli_DNI
+        {
+            get { return cli_DNI; }
+            set { cli_DNI = value; }
+        }
+        private int pro_Codigo;
+
+        public int Pro_Codigo
+        {
+            get { return pro_Codigo; }
+            set { pro_Codigo = value; }
+        }
+        private string but_Fila;
+
+        public string But_Fila
+        {
+            get { return but_Fila; }
+            set { but_Fila = value; }
+        }
+        private string but_Nro;
+
+        public string But_Nro
+        {
+            get { return but_Nro; }
+            set { but_Nro = value; }
+        }
     }
 }
